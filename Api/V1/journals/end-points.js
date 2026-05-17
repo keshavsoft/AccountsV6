b5aee3c48bc24859c3d88d69dcefd9b09f658fe5
keshavsoft <nknnkumar@yahoo.com>
2026-05-17 23:13:01 +0530

    distinct added

--- Api/V1/journals/end-points.js
+++ Api/V1/journals/end-points.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { getFunc } from "./ShowAll/controller.js";
+import { getFunc as Distinct } from "./Distinct/controller.js";
 
 const tableName = "journals";
 
 const router = express.Router();
-router.get('/ShowAll', (req, res) => getFunc({ res, inTableName : tableName}));
+router.get('/ShowAll', (req, res) => getFunc({ res, inTableName: tableName }));
+router.get('/Distinct', (req, res) => Distinct({ res, inTableName: tableName }));
 
 export { router };
